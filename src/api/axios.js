3d545474a93b59c8b7fc2094f06ea49ff6f86a87
Sankalp Sachan <sankalpsachan2007@gmail.com>
2025-12-24 10:12:03 +0530

    Bckend link added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ecommerce-website-backend-1-u1zz.onrender.com/api',
     withCredentials: true // Important for cookies
 });
 
